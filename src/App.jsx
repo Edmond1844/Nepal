e5e293d5bc56822e8327a88c876dd7d4b819a596
edmond dantes <pavel@MacBook-Air.local>
2026-04-12 21:21:17 +0300

    Начал делать функционал

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,28 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./app/pages/Home/Home";
 
 function App() {
-	const [lang, setLang] = useState(() => {
-		const saved = localStorage.getItem("language");
-		return saved === "en" || saved === "ru" ? saved : "ru";
-	});
+  const [lang, setLang] = useState(() => {
+    const saved = localStorage.getItem("language");
+    return saved === "en" || saved === "ru" ? saved : "ru";
+  });
 
-	useEffect(() => {
-		localStorage.setItem("language", lang);
-	}, [lang]);
+  useEffect(() => {
+    localStorage.setItem("language", lang);
+  }, [lang]);
 
-	const translations = {
-		en: languageEng.en,
-		ru: languageRus.ru,
-	};
+  const translations = {
+    en: languageEng.en,
+    ru: languageRus.ru,
+  };
 
-	return (
-		<Routes>
-			<Route
-				path="/"
-				element={
-					<Home
-						lang={lang}
-						setLang={setLang}
-						language={translations}
-					/>
-				}
-			/>
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<Home lang={lang} setLang={setLang} language={translations} />}
+      />
+    </Routes>
+  );
 }
 
 export default App;
--- src/app/components/layout/Header/Header.jsx
+++ src/app/components/layout/Header/Header.jsx
@@ -6,68 +6,85 @@ import styles from "./Header.module.css";
 import Icon from "../../ui/Icon/Icon.jsx";
 
 export default function Header({
-	lang,
-	setLang,
-	language,
-	colorTheme,
-	setColorTheme,
+  lang,
+  setLang,
+  language,
+  colorTheme,
+  setColorTheme,
 }) {
-	const navLinks = language[lang].navLinks;
-	function toggleTheme() {
-		if (colorTheme === "light") {
-			setColorTheme("dark");
-		} else if (colorTheme === "dark") {
-			setColorTheme("neon");
-		} else if (colorTheme === "neon") {
-			setColorTheme("light");
-		}
-	}
+  const navLinks = language[lang].navLinks;
+  function toggleTheme() {
+    if (colorTheme === "light") {
+      setColorTheme("dark");
+    } else if (colorTheme === "dark") {
+      setColorTheme("neon");
+    } else if (colorTheme === "neon") {
+      setColorTheme("light");
+    }
+  }
 
-	return (
-		<>
-			<header className={styles.header}>
-				<div className={`${styles.header__wrapper_content} container`}>
-					<Link to={"/"}>
-						<div className={styles.header__logo_icon} />
-					</Link>
-					<div className={styles.header__content}>
-						<nav className={styles.header__nav}>
-							<ul className={styles.header__nav_list}>
-								{navLinks.map((link) => (
-									<li key={link.to}>
-										<NavLink to={link.to}>
-											{link.text}
-										</NavLink>
-									</li>
-								))}
-							</ul>
-						</nav>
-						<div className={styles.header__buttons_wrapper}>
-							<button
-								className={`${styles.header__button} ${styles.header__button_language}`}
-								onClick={() =>
-									setLang(lang === "en" ? "ru" : "en")
-								}
-							>
-								{lang === "en"
-									? language.ru.language
-									: language.en.language}
-							</button>
-							<button
-								className={`${styles.header__button} flex items-center justify-center`}
-								onClick={toggleTheme}
-							>
-								<Icon name="icon-toggle-theme" />
-							</button>
-							<button
-								className={`${styles.header__button} flex items-center justify-center`}
-							>
-								<Icon name="icon-burger" />
-							</button>
-						</div>
-					</div>
-				</div>
-			</header>
-		</>
-	);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={`${styles.header__wrapper_content} container`}>
+          <Link to={"/"}>
+            <div className={`${styles.header__logo_wrapper} flex`}>
+              <Icon className={styles.header__logo_icon} name="icon-mountain" />
+              <span className={styles.header__logo_text}>Nepal Peak</span>
+            </div>
+          </Link>
+          <div className={styles.header__content}>
+            <nav className={styles.header__nav} aria-label="Main navigation">
+              <ul className={styles.header__nav_list}>
+                {navLinks.map((link) => (
+                  <li key={link.to}>
+                    <NavLink to={link.to}>{link.text}</NavLink>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+            <div className={styles.header__buttons_wrapper}>
+              <button
+                className={`${styles.header__button} ${styles.header__button_language}`}
+                onClick={() => setLang(lang === "en" ? "ru" : "en")}
+              >
+                {lang === "en" ? language.ru.language : language.en.language}
+              </button>
+              <button
+                className={`${styles.header__button} flex items-center justify-center`}
+                onClick={toggleTheme}
+              >
+                <Icon name="icon-toggle-theme" />
+              </button>
+              <button
+                className={`${styles.header__button} ${styles.header__button_burger} flex items-center justify-center`}
+                onClick={toggleMenu}
+              >
+                <Icon name="icon-burger" />
+              </button>
+              {isMenuOpen && (
+                <nav
+                  className={styles.header_mobile_menu}
+                  aria-label="Mobile navigation"
+                >
+                  <ul className={styles.header__mobile_menu_list}>
+                    {navLinks.map((link) => (
+                      <li key={link.to}>
+                        <NavLink to={link.to}>{link.text}</NavLink>
+                      </li>
+                    ))}
+                  </ul>
+                </nav>
+              )}
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
